d07b746412b65161c62cfd32386e70b6ac1f65e9
alish-mayor <alish22061999@gmail.com>
2022-01-02 21:47:35 +0600

    basic components added

---
+++ src/components/JokeCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="card">
+    <div>
+      <button class="card__btn">I want hear another one!</button>
+      <h2 class="card__title">One day dad said:</h2>
+    </div>
+    <p class="card__text"></p>
+    <p class="card__id"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      data: '',
+      url: 'https://icanhazdadjoke.com/',
+      options: {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        
+      }
+    }
+  },
+  methods: {
+    async loadJoke(){
+      try {
+        const response = await fetch(this.url, this.options);
+        if(!response.ok){
+          const msg = 'Nothing found';
+          throw new Error(msg);
+        }
+        this.data = await response.json();
+        console.log(this.data);
+      } catch(error){
+        console.log(error);
+      }
+    }
+  },
+  mounted(){
+    this.loadJoke();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <joke-card/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import JokeCard from '@/components/JokeCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    JokeCard,
   }
 }
 </script>
